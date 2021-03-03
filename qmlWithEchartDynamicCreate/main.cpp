9ce65161c0603ea4e94767200843998cb0bb6b1b
lpiert <33169234+lpiert@users.noreply.github.com>
2021-03-03 16:21:07 +0800

    append

---
+++ qmlWithEchartDynamicCreate/main.cpp
@@ -0,0 +1,39 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtWebEngine>
+#include <QApplication>
+#include "WebControl.h"
+#include "MainWindow.h"
+
+// 获取单例
+WebControl* getMySingleton(QQmlEngine *engine, QJSEngine *scriptEngine)
+{
+    Q_UNUSED(engine)
+    Q_UNUSED(scriptEngine)
+
+    return WebControl::instance();
+}
+
+
+// 注册单例到QML
+void qmlRegisterMySingleton()
+{
+    qmlRegisterSingletonType<WebControl>(
+                "WebControl", 1, 0, "WebControl", &getMySingleton);
+}
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QtWebEngine::initialize();
+    qmlRegisterMySingleton();
+    ads::CDockManager::setConfigFlag(ads::CDockManager::FocusHighlighting, true);
+    ads::CDockManager::setConfigFlag(
+                ads::CDockManager::AllTabsHaveCloseButton, true);
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return app.exec();
+}
